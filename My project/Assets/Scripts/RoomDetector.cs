048059a104ef85e041af8818fe04f791bbedf52f
ポッピコーン <43871609+popiicorn@users.noreply.github.com>
2026-06-28 14:44:29 +0900

    部屋の出入りセンサー実装

---
+++ My project/Assets/Scripts/RoomDetector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class RoomDetector : MonoBehaviour
+{
+    [Header("この部屋の番号")]
+    public int roomNumber;
+
+    // プレイヤーがセンサー（部屋）に入った瞬間に呼ばれる
+    void OnTriggerEnter(Collider other)
+    {
+        // ぶつかった相手のTagが「Player」だったら
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("プレイヤーが 【部屋 " + roomNumber + "】 に入りました！");
+        }
+    }
+
+    // プレイヤーがセンサー（部屋）から出た瞬間に呼ばれる
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("プレイヤーが 【部屋 " + roomNumber + "】 から出ました！");
+        }
+    }
+}
